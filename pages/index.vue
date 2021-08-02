36413dbab27f0255b656063380eb12d719fd2b8a
jenyckee <jeroen.nyckees@gmail.com>
2021-08-02 15:52:30 +0200

    firsts commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="vl-button">
+    <span class="vl-button__label">Gegevens opslaan</span>
+  </button>
+</template>
+
+<script>
+
+export default {}
+</script>
+
+<style lang="scss">
+    @import "~@govflanders/vl-ui-core/src/scss/core";
+    @import "~@govflanders/vl-ui-button/src/scss/button";
+</style>
