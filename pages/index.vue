8d064b0f427b8381757e315483cbb0014542e6a5
Jeroen Nyckees <jeroen.nyckees@gmail.com>
2021-12-22 10:23:42 +0100

    clarify path trimming

--- pages/index.vue
+++ pages/index.vue
@@ -8,15 +8,22 @@
 
 <script>
 
+function trimPath(path) {
+  /* Nuxt considers by default the full path:
+  * for example /report/doc/vocabularium/documentatie/associaties-voc-met-uri-tags/html-nj_nl
+  * the actual path should be however /doc/vocabularium/documentatie/associaties-voc-met-uri-tags/
+  */
+  return path.slice(8, -11)
+}
+
 export default {
   async asyncData ({ $content }) {
     const content = await $content('report', { deep: true }).fetch()
-
     return {
       content: content.filter(page => !page.path.includes('translation')).map(page => {
         return ({
         ...page,
-        path: page.path.slice(8, -11)
+        path:  trimPath(page.path)
       })})
     }
   }
