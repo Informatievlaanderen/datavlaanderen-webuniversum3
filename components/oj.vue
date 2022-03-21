c9807b143715586b5a111055fd18932d7a47e5ed
Jeroen Nyckees <jeroen.nyckees@gmail.com>
2022-03-21 09:52:18 +0100

    treeview

---
+++ components/oj.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="vl-region">
+    <vl-grid>
+      <div class="vl-col vl-col--4-6">
+        <div class="js-vl-scrollspy__content">
+          <div class="vl-layout vl-layout--wide">
+            <div class="grid">
+              <div class="vl-col--8-12 vl-col--12-12--s">
+                <generic-meta :profile="profile"></generic-meta>
+                <ap-introduction
+                  :profile="profile"
+                  :language="language"
+                ></ap-introduction>
+                <client-only placeholder="Loading...">
+                  <treeview :treeData="profile.metadata.documentconfig.treeData"></treeview>
+                </client-only>
+                <ap-summary
+                  :profile="profile"
+                  :language="language"
+                ></ap-summary>
+                <ap-status :profile="profile" :language="language"></ap-status>
+                <generic-license
+                  :profile="profile"
+                  :language="language"
+                ></generic-license>
+                <ap-conformance
+                  :profile="profile"
+                  :language="language"
+                ></ap-conformance>
+                <ap-overview
+                  :profile="profile"
+                  :language="language"
+                ></ap-overview>
+                <ap-jsonld :profile="profile" :language="language"></ap-jsonld>
+                <ap-shacl :profile="profile" :language="language"></ap-shacl>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="vl-col vl-col--2-6 vl-col--1-1--s">
+        <ap-side-nav
+          :hasdescription="profile.metadata.description"
+        ></ap-side-nav>
+      </div>
+    </vl-grid>
+  </div>
+</template>
+<script>
+import treeview from './treeview.vue';
+export default {
+  components: { treeview },
+  props: ["profile", "language"],
+};
+</script>
---
+++ components/unknown.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="vl-region">
+    <h1>Unknown document type: {{documenttype}}</h1>
+  </div>
+</template>
+<script>
+export default {
+};
+</script>
